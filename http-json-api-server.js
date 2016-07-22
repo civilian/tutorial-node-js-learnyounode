e812d6de2b45fbe7992a22da7026b189568c33bf
civilian <chamatoscar@gmail.com>
2016-07-22 10:35:39 -0500

    Finished course.

---
+++ http-json-api-server.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var url = require('url');
+
+var server = http.createServer(function (req, res){
+    if(req.method != 'GET')
+        return res.end('Send me a GET\n');
+
+    parsedUrl = url.parse(req.url, true);
+    ansObj = {};
+    var date = new Date(parsedUrl.query.iso);
+    
+    if(parsedUrl.pathname === '/api/parsetime'){
+        ansObj = { "hour": date.getHours(),
+            "minute": date.getMinutes(),
+            "second": date.getSeconds()
+        };
+
+    } else if (parsedUrl.pathname === '/api/unixtime') {
+        ansObj = {"unixtime": date.getTime()};
+    }
+
+    ansString = JSON.stringify(ansObj);
+    res.writeHead(200, { 'Content-Type':'application/json' });
+    res.end(ansString);
+
+});
+
+server.listen(process.argv[2]);
